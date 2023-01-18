de790c4dc9a46f547b2dea263693b0e0d543dd2d
zhapar <zhapar28@gmail.com>
2023-01-18 13:49:47 -0600

    some tweak

--- features/Header.js
+++ features/Header.js
@@ -1,68 +1,61 @@
-import Link from 'next/link'
+import Link from "next/link"
 import {
-  useGlobalDispatchContext,
-  useGlobalStateContext,
-} from 'context/globalContext'
-import useElementPosition from 'libs/useElementPosition'
-import { useRef } from 'react'
+    useGlobalDispatchContext,
+    useGlobalStateContext,
+} from "context/globalContext"
+import useElementPosition from "libs/useElementPosition"
+import { useRef } from "react"
 
 function Header({ setToggleMenu, setFixedCursorPosition }) {
-  const { currentTheme } = useGlobalStateContext()
-  const { dispatch, onCursor } = useGlobalDispatchContext()
-  const hamburger = useRef(null)
-  const position = useElementPosition(hamburger)
+    const { currentTheme } = useGlobalStateContext()
+    const { dispatch, onCursor } = useGlobalDispatchContext()
+    const hamburger = useRef(null)
+    const position = useElementPosition(hamburger)
 
-  function toggleTheme() {
-    if (currentTheme === 'dark') {
-      dispatch({ type: 'TOGGLE_THEME', currentTheme: 'light' })
-    } else if (currentTheme === 'light') {
-      dispatch({ type: 'TOGGLE_THEME', currentTheme: 'dark' })
+    function toggleTheme() {
+        if (currentTheme === "dark") {
+            dispatch({ type: "TOGGLE_THEME", currentTheme: "light" })
+        } else if (currentTheme === "light") {
+            dispatch({ type: "TOGGLE_THEME", currentTheme: "dark" })
+        }
     }
-  }
 
-  function menuHover() {
-    onCursor('locked')
-    setFixedCursorPosition({ x: position.x, y: position.y + 20 })
-  }
+    function menuHover() {
+        onCursor("locked")
+        setFixedCursorPosition({ x: position.x, y: position.y + 20 })
+    }
 
-  return (
-    <div className="h-0 w-full absolute top-5 sm:top-[72px] right-0 left-0 z-[99]">
-      <div className="container">
-        <div className="flex justify-between h-0">
-          <div
-            className="mt-[15px]"
-            onMouseEnter={() => onCursor('hovered')}
-            onMouseLeave={onCursor}>
-            <Link href="/">
-              <a className="text-[1.8rem] no-underline font-extrabold text-theme pr-[4px]">
-                FURR
-              </a>
-            </Link>
-            <span
-              className="h-[16px] w-[16px] bg-primary rounded-full inline-block relative bottom-[2px]"
-              onMouseEnter={() => onCursor('pointer')}
-              onMouseLeave={() => onCursor('hovered')}
-              onClick={toggleTheme}
-            />
-            <Link href="/">
-              <a className="text-[1.8rem] no-underline font-extrabold text-theme pl-[4px]">
-                W
-              </a>
-            </Link>
-          </div>
-          <button
-            ref={hamburger}
-            className="origin-center border-none py-5 sm:p-[20px] bg-none outline-none"
-            onClick={() => setToggleMenu(true)}
-            onMouseEnter={menuHover}
-            onMouseLeave={onCursor}>
-            <span className="w-[36px] h-[8px] block -bg-theme m-[8px]"></span>
-            <span className="w-[36px] h-[8px] block -bg-theme m-[8px]"></span>
-          </button>
+    return (
+        <div className="h-0 w-full absolute top-5 sm:top-[72px] right-0 left-0 z-[99]">
+            <div className="container">
+                <div className="flex justify-between h-0">
+                    <div
+                        className="mt-[15px]"
+                        onMouseEnter={() => onCursor("hovered")}
+                        onMouseLeave={onCursor}>
+                        <Link href="/">
+                            <a className="text-[1.8rem] no-underline font-extrabold text-theme pr-[4px]">
+                                <img
+                                    src="/assets/svg/logo.svg"
+                                    alt="aKumoSolutions logo"
+                                    className="w-28"
+                                />
+                            </a>
+                        </Link>
+                    </div>
+                    <button
+                        ref={hamburger}
+                        className="origin-center border-none py-5 sm:p-[20px] bg-none outline-none"
+                        onClick={() => setToggleMenu(true)}
+                        onMouseEnter={menuHover}
+                        onMouseLeave={onCursor}>
+                        <span className="w-[36px] h-[8px] block -bg-theme m-[8px]"></span>
+                        <span className="w-[36px] h-[8px] block -bg-theme m-[8px]"></span>
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Header
--- features/HomeBanner.js
+++ features/HomeBanner.js
@@ -1,136 +1,136 @@
-import { useRef, useEffect, useState } from 'react'
-import { motion } from 'framer-motion'
-import useWindowSize from 'libs/useWindowSize'
+import { useRef, useEffect, useState } from "react"
+import { motion } from "framer-motion"
+import useWindowSize from "libs/useWindowSize"
 import {
-  useGlobalDispatchContext,
-  useGlobalStateContext,
-} from 'context/globalContext'
+    useGlobalDispatchContext,
+    useGlobalStateContext,
+} from "context/globalContext"
 
 function HomeBanner() {
-  const [mounted, setMounted] = useState(false)
+    const [mounted, setMounted] = useState(false)
 
-  const size = useWindowSize()
-  const { currentTheme } = useGlobalStateContext()
-  const { onCursor } = useGlobalDispatchContext()
-  let canvas = useRef(null)
+    const size = useWindowSize()
+    const { currentTheme } = useGlobalStateContext()
+    const { onCursor } = useGlobalDispatchContext()
+    let canvas = useRef(null)
 
-  useEffect(() => {
-    if (mounted) {
-      let renderingElement = canvas.current
+    useEffect(() => {
+        if (mounted) {
+            let renderingElement = canvas.current
 
-      // offscreen canvas for only drawing
-      let drawingElement = renderingElement.cloneNode()
-      let drawingCtx = drawingElement.getContext('2d')
-      let renderingCtx = renderingElement.getContext('2d')
-      let lastX
-      let lastY
-      let moving = false
+            // offscreen canvas for only drawing
+            let drawingElement = renderingElement.cloneNode()
+            let drawingCtx = drawingElement.getContext("2d")
+            let renderingCtx = renderingElement.getContext("2d")
+            let lastX
+            let lastY
+            let moving = false
 
-      renderingCtx.globalCompositeOperation = 'source-over'
-      // to do
-      renderingCtx.fillStyle = currentTheme === 'dark' ? '#000' : '#fff'
-      renderingCtx.fillRect(0, 0, size.width, size.height)
+            renderingCtx.globalCompositeOperation = "source-over"
+            // to do
+            renderingCtx.fillStyle = currentTheme === "dark" ? "#000" : "#fff"
+            renderingCtx.fillRect(0, 0, size.width, size.height)
 
-      renderingElement.addEventListener('mouseover', (event) => {
-        moving = true
-        lastX = event.pageX - renderingElement.offsetLeft
-        lastY = event.pageY - renderingElement.offsetTop
-      })
+            renderingElement.addEventListener("mouseover", (event) => {
+                moving = true
+                lastX = event.pageX - renderingElement.offsetLeft
+                lastY = event.pageY - renderingElement.offsetTop
+            })
 
-      renderingElement.addEventListener('click', (event) => {
-        moving = true
-        lastX = event.pageX - renderingElement.offsetLeft
-        lastY = event.pageY - renderingElement.offsetTop
-      })
+            renderingElement.addEventListener("click", (event) => {
+                moving = true
+                lastX = event.pageX - renderingElement.offsetLeft
+                lastY = event.pageY - renderingElement.offsetTop
+            })
 
-      renderingElement.addEventListener('mouseup', (event) => {
-        moving = false
-        lastX = event.pageX - renderingElement.offsetLeft
-        lastY = event.pageY - renderingElement.offsetTop
-      })
+            renderingElement.addEventListener("mouseup", (event) => {
+                moving = false
+                lastX = event.pageX - renderingElement.offsetLeft
+                lastY = event.pageY - renderingElement.offsetTop
+            })
 
-      renderingElement.addEventListener('mousemove', (event) => {
-        if (moving) {
-          drawingCtx.globalCompositeOperation = 'source-over'
-          renderingCtx.globalCompositeOperation = 'destination-out'
-          let currentX = event.pageX - renderingElement.offsetLeft
-          let currentY = event.pageY - renderingElement.offsetTop
-          drawingCtx.lineJoin = 'round'
-          drawingCtx.moveTo(lastX, lastY)
-          drawingCtx.lineTo(currentX, currentY)
-          drawingCtx.closePath()
-          drawingCtx.lineWidth = 120
-          drawingCtx.stroke()
-          lastX = currentX
-          lastY = currentY
-          renderingCtx.drawImage(drawingElement, 0, 0)
+            renderingElement.addEventListener("mousemove", (event) => {
+                if (moving) {
+                    drawingCtx.globalCompositeOperation = "source-over"
+                    renderingCtx.globalCompositeOperation = "destination-out"
+                    let currentX = event.pageX - renderingElement.offsetLeft
+                    let currentY = event.pageY - renderingElement.offsetTop
+                    drawingCtx.lineJoin = "round"
+                    drawingCtx.moveTo(lastX, lastY)
+                    drawingCtx.lineTo(currentX, currentY)
+                    drawingCtx.closePath()
+                    drawingCtx.lineWidth = 120
+                    drawingCtx.stroke()
+                    lastX = currentX
+                    lastY = currentY
+                    renderingCtx.drawImage(drawingElement, 0, 0)
+                }
+            })
         }
-      })
+    }, [mounted, currentTheme])
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    const container = {
+        initial: { y: 800 },
+        animate: {
+            y: 0,
+            transition: {
+                staggerChildren: 0.2,
+            },
+        },
     }
-  }, [mounted, currentTheme])
 
-  useEffect(() => {
-    setMounted(true)
-  }, [])
+    const item = {
+        initial: { y: 800 },
+        animate: {
+            y: 0,
+            transition: {
+                duration: 1,
+                ease: [0.6, 0.05, -0.01, 0.9],
+            },
+        },
+    }
 
-  const container = {
-    initial: { y: 800 },
-    animate: {
-      y: 0,
-      transition: {
-        staggerChildren: 0.2,
-      },
-    },
-  }
-
-  const item = {
-    initial: { y: 800 },
-    animate: {
-      y: 0,
-      transition: {
-        duration: 1,
-        ease: [0.6, 0.05, -0.01, 0.9],
-      },
-    },
-  }
-
-  return (
-    <div className="bg-theme h-screen w-full relative mb-[100px] sm:mb-[296px]">
-      <div className="h-full w-full">
-        <video
-          src="/assets/video/video.mp4"
-          className="w-full h-full object-cover"
-          loop
-          autoPlay
-          muted
-        />
-      </div>
-      <canvas
-        ref={canvas}
-        height={mounted ? size.height : 300}
-        width={mounted ? size.width : 300}
-        onMouseEnter={() => onCursor('hovered')}
-        onMouseLeave={onCursor}
-        className="absolute top-0 left-0 h-full hidden sm:block"
-      />
-      <motion.h1
-        variants={container}
-        initial="initial"
-        animate="animate"
-        className="absolute bottom-0 md:bottom-[-120px] left-0 md:left-[-18px] text-theme pointer-events-none">
-        <motion.span
-          variants={item}
-          className="block text-[8rem] md:text-[23rem] font-black leading-[0.76]">
-          DIG
-        </motion.span>
-        <motion.span
-          variants={item}
-          className="block text-[8rem] md:text-[23rem] font-black leading-[0.76]">
-          DEEP
-        </motion.span>
-      </motion.h1>
-    </div>
-  )
+    return (
+        <div className="bg-theme h-screen w-full relative mb-[100px] sm:mb-[296px]">
+            <div className="h-full w-full">
+                <video
+                    src="/assets/video/akumosolutions.mov"
+                    className="w-full h-full object-cover"
+                    loop
+                    autoPlay
+                    muted
+                />
+            </div>
+            <canvas
+                ref={canvas}
+                height={mounted ? size.height : 300}
+                width={mounted ? size.width : 300}
+                onMouseEnter={() => onCursor("hovered")}
+                onMouseLeave={onCursor}
+                className="absolute top-0 left-0 h-full hidden sm:block"
+            />
+            <motion.h1
+                variants={container}
+                initial="initial"
+                animate="animate"
+                className="absolute bottom-0 md:bottom-[-120px] left-0 md:left-[-18px] text-theme pointer-events-none">
+                <motion.span
+                    variants={item}
+                    className="block text-[8rem] md:text-[19rem] font-black leading-[0.76] text-blue-400">
+                    DIG
+                </motion.span>
+                <motion.span
+                    variants={item}
+                    className="block text-[8rem] md:text-[19rem] font-black leading-[0.76] text-blue-400">
+                    DEEP
+                </motion.span>
+            </motion.h1>
+        </div>
+    )
 }
 
 export default HomeBanner
